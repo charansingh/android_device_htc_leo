338d768a6608bbfb40de6ccd76382ab5eb4044c0
charansingh <charansingh.online@gmail.com>
2011-01-24 16:46:24 +0530

    Fix sensors and add board-info.txt

--- libsensors/Android.mk
+++ libsensors/Android.mk
@@ -21,22 +21,13 @@ ifneq ($(TARGET_SIMULATOR),true)
 # hw/<SENSORS_HARDWARE_MODULE_ID>.<ro.product.board>.so
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := sensors.leo
+LOCAL_MODULE := sensors.htcleo
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_CFLAGS := -DLOG_TAG=\"Sensors\"
-LOCAL_SRC_FILES := 						\
-				sensors.c 				\
-				nusensors.cpp 			\
-				InputEventReader.cpp	\
-				SensorBase.cpp			\
-				LightSensor.cpp			\
-				ProximitySensor.cpp		\
-				AkmSensor.cpp
-
+LOCAL_SRC_FILES := sensors.c
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_PRELINK_MODULE := false
 
